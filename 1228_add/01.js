9169acf595e6b51941d8c09bb9110cde7cf7b830
ayeon min <aktyu101@naver.com>
2022-12-28 23:41:18 +0900

    add: study - 1228

---
+++ 1228_add/01.js
@@ -0,0 +1,11 @@
+console.log('** 1224 과제 **')
+
+window.addEventListener('load', => {
+    //5*5
+    //i=1, (i++)*5 삼각형
+    //i=5, (i--)*5 역삼각형
+    //정삼각형
+    //역정삼각형
+    //다이아몬드
+});
+console.log('** 1224 과제 - end **')
--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 import "./1224/index.js"
+import "./1228_add/index.js"
