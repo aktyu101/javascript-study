85d55533c9448416e35902c0fcc5874c55948b8d
ayeon min <aktyu101@naver.com>
2022-12-28 23:31:15 +0900

    add: study - 1224

--- 1217_add/01.js
+++ 1217_add/01.js
@@ -4,7 +4,7 @@ console.log('** 1217 과제 **')
 const a = [1, 2, 3, 4, 5];
 const b = [...a, 6, 7];
 
-console.log(a,b);
+console.log(b);
 
 /**사람의 이름과 나이를 정보로 가진 변수 classA가 있습니다. 변수 classB에 classA 정보를 복사한 후,
 26살의 kein을 추가해보세요.*/
@@ -18,7 +18,12 @@ const classA = {
 	}
 };
 
-const classB = {...classA, "kein" : {age : 26}};
+const classB = {
+	...classA,
+	 "kein" : {
+		age : 26
+	}
+};
 console.log(classB);
 /**
  * 숫자 배열을 가진 변수 c가 있습니다. 변수 d에 복사한 후 9, 10을 추가하고 숫자 1, 2는 각각 10과 20으로 변경하세요.
@@ -47,13 +52,24 @@ const classA2 = {
 	}
 };
 // 변수 classB에 kein: { age: 27 } 추가
-const classB2 = Object.assign({}, classA2, { judy: {age : 26}}, { pillip: {age : 25}, "kein": {age : 27}});
-console.log(classB2);
+const classB2 = {
+	judy: {
+		...classA2.judy,
+		age: 26
+	},
+	pillip: {
+		...classA2.pillip,
+		age: 25
+	},
+	kein: {
+		age: 27,
+		email: 'pillip@gmail.com'
+	}
+};
+//const classB2 = Object.assign({}, classA2, { judy: {age : 26}}, { pillip: {age : 25}, "kein": {age : 27}});
+//console.log(classB2);
 
 // classB 학생들 정보에는 name@gmail.com 형태의 email 정보 각각 추가
-classB2.judy.email = "judy@gmail.com";
-classB2.pillip.email = "pillip@gmail.com";
-classB2.kein.email = "kein@gmail.com";
 
 console.log(classB2);
 //반복문 과제
@@ -61,30 +77,30 @@ console.log(classB2);
 const a3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(a3);
 
-for (let i = 1; i <= a3.length; i++ ) {
-    console.log(i);
+for (let i = 0; i < a3.length; i++ ) {
+    console.log(a[i]);
 }
 //반대로 출력해 보세요.
-for (let i = a3.length; i >= 0; i-- ) {
-    console.log(i);  //a3[i]
+for (let i = a3.length - 1; i >= 0; i-- ) {
+    console.log(a[i]);  //a3[i]
 }
 //짝수만 출력해 보세요.
-for (let i = 1; i <= a3.length; i++ ) {
+for (let i = 0; i < a3.length; i++ ) {
 		//const num = a[i];
-	if(i % 2 === 0) {
-		console.log("짝수", i);
+	if(a3[i] % 2 !== 0) continue; {
+		console.log("짝수", a3[i]);
 	}
 }
 //3과 5가 아닌 수만 출력해 보세요.
 for (let i = 0; i <= a3.length; i++ ) {
-	if(i !== 3 && i !== 5) {
-		console.log("3과 5가 아닌 수", i);
+	if(a3[i] !== 3 && a3[i] !== 5) continue; { //3, 5라면 무시
+		console.log("3과 5가 아닌 수", a3[i]);
 	}
 }
 //5의 배수만 출력해 보세요.
-for (let i = 1; i <= a3.length; i++ ) {
-	if( (i % 5) === 0 ) {
-		console.log("5의 배수", i);
+for (let i = 0; i <= a3.length; i++ ) {
+	if(a3[i] % 5 !== 0 ) continue; { //5의 배수라면 무시 
+		console.log("5의 배수", a3[i]);
 	}
 }
 //if (num === 3 || num === 5) continue;
@@ -94,30 +110,40 @@ for (let i = 1; i <= a3.length; i++ ) {
 const obj = { a: 1, b: 2, c: 3 };
 //key a, b, c만 출력해 보세요.
 
-for ( let key of Object.keys(obj)){
-	console.log(key);
+const keyArr = Object.keys(obj);
+for ( let k of keyArr ){
+	console.log(k);
 }
-const keys = Object.keys(obj);
-console.log(keys);
+
 //value 1, 2, 3만 출력해 보세요.
-const values = Object.values(obj);
-console.log(values);
+const valueArr = Object.values(obj);
+for ( let v of valueArr ){
+	console.log(v);
+};
 //entry에 해당하는 [key, value] 형태를 출력해 보세요.
-const entries = Object.entries(obj);
-console.log(entries);
+const entryArr = Object.entries(obj);
+for (let [k, v] of entryArr) {
+	console.log('key', k);
+	console.log('value', v);
+};
 
 //key: a를 제외하고 출력해 보세요.
-for (let [k, v] of ) {
-	if(){
+const keyArr2 = Object.keys(obj);
+for (let k of keyArr2) {
+	if(k === 'a') continue; {
 		console.log('for of', k);
 	}
-}
+};
 //value가 짝수인 경우만 출력해 보세요.
-
+const valueArr2 = Object.values(obj);
+for (let v of valueArr2) {
+	if (v % 2 !== 0) continue;
+	console.log(v);
+};
 
 //a, b, c의 총합을 변수 sum에 담아 출력해 보세요.
 let sum = 0; //외부변수가 필요함
-for (let value of valArr){
+for (let value of valArr3){
 	sum += value;
-}
+};
 console.log('** 1217 과제 - end **')
