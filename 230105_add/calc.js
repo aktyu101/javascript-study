2fe9c2c4d7abf6902e3602a03d7dc3dcdea6ec24
aktyu101 <aktyu101@gmail.com>
2023-01-07 10:00:44 +0900

    230107 : study

--- 230105_add/calc.js
+++ 230105_add/calc.js
@@ -15,7 +15,7 @@ const Btn6 = document.querySelector(".num:nth-child(6)");
 const Btn1 = document.querySelector(".num:nth-child(7)");
 const Btn2 = document.querySelector(".num:nth-child(8)");
 const Btn3 = document.querySelector(".num:nth-child(9)");
-const dot = document.querySelector(".num:nth-child(10)");
+const Btn0 = document.querySelector(".num:nth-child(10)");
 
 const log = document.querySelector("#log");
 
@@ -26,7 +26,7 @@ input.value = num;
 
 const finishCalcrator = (num) => {
     log.innerHTML = num;
-    //input.value = 0;
+    input.value = 0;
 };
 
 // const finishCalc = (num) => {
@@ -60,6 +60,85 @@ const allReset = () => {
     log.innerHTML = num = 0 ;
 };
 
+
+// const input7 = () => {
+//     if (num === 0) {
+//         input.value = num = 7;
+//     }else {
+//         input.value = num += 7;
+//     };
+// };
+// const input8 = () => {
+//     if (num === 0) {
+//         input.value = num = 8;
+//     }else {
+//         input.value += num = 8;
+//     };
+// };
+// const input9 = () => {
+//     if (num === 0) {
+//         input.value = num = 9;
+//     }else {
+//         input.value += num = 9;
+//     };
+// };
+// const input4 = () => {
+//     if (num === 0) {
+//         input.value = num = 4;
+//     }else {
+//         input.value += num = 4;
+//     };
+// };
+// const input5 = () => {
+//     if (num === 0) {
+//         input.value = num = 5;
+//     }else {
+//         input.value += num = 5;
+//     };
+// };
+// const input6 = () => {
+//     if (num === 0) {
+//         input.value = num = 6;
+//     }else {
+//         input.value += num = 6;
+//     };
+// };
+// const input1 = () => {
+//     if (num === 0) {
+//         input.value = num = 1;
+//     }else {
+//         input.value += num = 1;
+//     };
+// };
+// const input2 = () => {
+//     if (num === 0) {
+//         input.value = num = 2;
+//     }else {
+//         input.value += num = 2;
+//     };
+// };
+// const input3 = () => {
+//     if (num === 0) {
+//         input.value = num = 3;
+//     }else {
+//         input.value += num = 3;
+//     };
+// };
+
+
+
+//숫자
+// Btn7.addEventListener("click", input7);
+// Btn8.addEventListener("click", input8);
+// Btn9.addEventListener("click", input9);
+// Btn4.addEventListener("click", input4);
+// Btn5.addEventListener("click", input5);
+// Btn6.addEventListener("click", input6);
+// Btn1.addEventListener("click", input1);
+// Btn2.addEventListener("click", input2);
+// Btn3.addEventListener("click", input3);
+
+//calc
 plusBtn.addEventListener("click", plus);
 minusBtn.addEventListener("click", minus);
 multiBtn.addEventListener("click", multi);
