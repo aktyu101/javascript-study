06e0e1be9331a0a367e8236ac7f6902e1181b57d
aktyu101 <aktyu101@gmail.com>
2023-02-05 23:54:29 +0900

    230205 : study_add

---
+++ 230205_add/index.js
@@ -0,0 +1,32 @@
+const people = [
+  { name: "chris", age: 28, address: "seoul" },
+  { name: "judy", age: 45, address: "incheon" },
+  { name: "mike", age: 11, address: "bucheon" },
+  { name: "tom", age: 55, address: "busan" },
+  { name: "sara", age: 35, address: "jeju" },
+];
+
+//map
+const map = people.map((value, index) => {
+  return {
+    id: index,
+    nick: value.name,
+    age: value.age,
+    address: value.address,
+  };
+});
+console.log("map", map);
+
+//forEach
+let count = 0;
+let str = "";
+people.forEach((value, index) => {
+  console.log("forEach index", index); //for, forof랑 기능이 동일함 -> forEach
+  str += `${value.name} : ${value.address}`;
+  count += value.age;
+});
+
+//forEach
+people.forEach((value, index) => {
+  console.log("forEach", index, value);
+});
